47f9828274e1475cb780700af429a52ecffd6bfe
timkershaw <tkershaw403@gmail.com>
2020-06-23 20:22:34 -0400

    Part of Unit Test Project

---
+++ src/junitproject/MyAgileTest.java
@@ -0,0 +1,13 @@
+
+package junitproject;
+
+
+public class MyAgileTest {
+    public int add (String s1, String s2) {
+        int int1 = Integer.parseInt(s2);
+        int int2 = Integer.parseInt(s2);
+        
+        return int1 + int2;
+    }
+    
+}
